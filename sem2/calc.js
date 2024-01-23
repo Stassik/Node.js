77f158d9f9c149f848083da9245acf8690a0de66
unknown <nastase4ka@mail.ru>
2024-01-23 23:03:06 +0300

    add hw2 folder

---
+++ sem2/calc.js
@@ -0,0 +1,10 @@
+const NP = require('number-precision');
+
+function calculateResultSum(purchases, discount) {
+    let total = purchases.reduce((acc, purchase) => acc += purchase, 0);
+    total = NP.times(total, discount);
+    return total;
+}
+
+
+module.exports = calculateResultSum;
