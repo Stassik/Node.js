493fff1add9424ffeb9cebb764d14599aefb5dc1
unknown <nastase4ka@mail.ru>
2024-01-24 13:55:12 +0300

    mod module

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Equation = require("./equation.js");
-/*Пимер использования*/
+const discriminantSearch = require("./disc.js");
 
-const newF = new Equation(-1, 2, 8); // -x^2 + 2x + 8 = 0;
-console.log(newF.getDisc()); // Вывод в консоль значение Дискриминанта
-console.log(newF.rootSearch()); // Вывод в консоль результат решения
+
+// /*Пример использования*/
+
+// const newF = new Equation(-1, 2, 8); // -x^2 + 2x + 8 = 0;
+// console.log(newF.getDisc()); // Вывод в консоль значение Дискриминанта
+// console.log(newF.rootSearch()); // Вывод в консоль результат решения
