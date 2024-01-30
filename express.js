889d3922c2d064029dec4e00bc3b7a250c7205fe
unknown <nastase4ka@mail.ru>
2024-01-30 19:27:34 +0300

    add hw 3

--- express.js
+++ express.js
@@ -1,7 +1,39 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
-app.use(express.static('static'));
 
+const file = JSON.parse(fs.readFileSync('static/views.json'));
+
+
+app.get('/', updateCount, getCount, (req, res) => {
+    res.send(`
+    <h1>Главная страница</h1>
+    <a href="/about">Ссылка на страницу обо мне</a>
+    <p>Эту страницу посетили ${req.counter} раз.</p>`);
+});
+app.get('/about', updateCount, getCount, (req, res) => {
+    res.send(`
+    <h1>Страница обо мне</h1>
+    <a href="/">Ссылка на гланвую страницу</a>
+    <p>Эту страницу посетили ${req.counter} раз.</p>`);
+});
 
 
 app.listen(3000);
+
+
+function updateCount(req, res, next) {
+    const url = req.url;
+    file[url] = file[url] + 1;
+    const pathToFile = path.join(__dirname, 'static/views.json');
+    const viewsJson = JSON.stringify(file, null, 2);
+    fs.writeFileSync(pathToFile, viewsJson);
+    next();
+}
+
+function getCount(req, res, next) {
+    const url = req.url;
+    req.counter = file[url];
+    next();
+}
